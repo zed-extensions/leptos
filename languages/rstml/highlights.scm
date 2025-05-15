095b848d820200be5d6a189d79ec90660dbeee00
Ben Kunkle <ben.kunkle@gmail.com>
2025-05-15 10:25:20 +0200

    improve highlights

--- languages/rstml/highlights.scm
+++ languages/rstml/highlights.scm
@@ -1,9 +1,8 @@
 ; JSX elements
-; (open_tag name: (node_identifier (identifier) @tag.jsx (#match? @tag.jsx "^[a-z][^.]*$")) )
-; (close_tag (identifier) @tag.jsx (#match? @tag.jsx "^[a-z][^.]*$"))
-; (self_closing_element_node (identifier) @tag.jsx (#match? @tag.jsx "^[a-z][^.]*$"))
+(open_tag name: (node_identifier (identifier) @tag.jsx (#match? @tag.jsx "^[a-z][^.]*$")) )
+(close_tag name: (node_identifier) @tag.jsx (#match? @tag.jsx "^[a-z][^.]*$"))
+(self_closing_element_node name: (node_identifier) @tag.jsx (#match? @tag.jsx "^[a-z][^.]*$"))
 
-; (jsx_attribute (property_identifier) @attribute.jsx)
 (node_attribute (node_identifier) @attribute.jsx)
 (open_tag (["<" ">"]) @punctuation.bracket.jsx)
 (close_tag (["</" ">"]) @punctuation.bracket.jsx)
